2425941dc1e64424cf9a76445b6b0ad6ac36e095
zhangtongzheng <207201595@qq.com>
2022-12-16 16:22:18 +0800

    2022-12-16commit01

--- src/main/java/com/itheima/ReggitTakeOutApplication.java
+++ src/main/java/com/itheima/ReggitTakeOutApplication.java
@@ -16,7 +16,6 @@ public class ReggitTakeOutApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggitTakeOutApplication.class, args);
         log.info("项目启动成功");
-        log.info("测试push成功");
 
 
     }
