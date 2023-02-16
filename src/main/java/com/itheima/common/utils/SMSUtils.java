f4d4fa7f0bc3ada746aa8d0b8b43c9dd98a2d00e
zhangtongzheng <207201595@qq.com>
2023-02-17 06:47:10 +0800

    前后端分离部署

--- src/main/java/com/itheima/common/utils/SMSUtils.java
+++ src/main/java/com/itheima/common/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tRjLtFJDQ2KopVDhDNi", "DIySiy0wkeWcEBFLhmbhEU6JP32nkf");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "XX", "XX");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
